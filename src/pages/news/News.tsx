7cb7457f77ca9cf1c7b3a88e94f986284eedf1e1
Amir.Thp <code2time@gmail.com>
2024-03-16 14:07:55 -0700

    create basic news page

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,12 +1,21 @@
-import Navbar from '../../components/navbar/Navbar'
-import './News.css'
+import Navbar from "../../components/navbar/Navbar";
+import "./News.css";
 
 function News() {
   return (
     <>
-    <Navbar />
+      <div className="background">
+        <Navbar />
+      </div>
+    <div className="news-container max-w-[999px] h-[800px] w-5/6 mx-auto my-48 border grid grid-cols-12  ">
+      <div className="left-item col-span-3 w-full h-full border" > left item</div>
+      <div className="center-item col-span-6 w-full h-full border" ></div>
+      <div className="right-item col-span-3 w-full h-full border" >right item</div>
+
+    </div>
+     
     </>
-  )
+  );
 }
 
-export default News
+export default News;
