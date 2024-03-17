49072bdf057d675ef141d4dee4a5824cdacc851c
Amir.Thp <code2time@gmail.com>
2024-03-17 13:37:12 -0700

    create section1 in news page

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar/Navbar";
 import "./News.css";
+import gta from "../../assets/videos/gta.mp4";
 
 function News() {
   return (
@@ -7,13 +8,17 @@ function News() {
       <div className="background">
         <Navbar />
       </div>
-    <div className="news-container max-w-[999px] h-[800px] w-5/6 mx-auto my-48 border grid grid-cols-12  ">
-      <div className="left-item col-span-3 w-full h-full border" > left item</div>
-      <div className="center-item col-span-6 w-full h-full border" ></div>
-      <div className="right-item col-span-3 w-full h-full border" >right item</div>
 
-    </div>
-     
+      <div className="news-container max-w-[999px] h-auto p-5 w-5/6 mx-auto my-12  grid grid-cols-12  ">
+        <h1 className="title-gta right-12 mt-12 absolute text-slate-200 Lalezar  ">!منتشر شد <span className="text-xl ">GTA</span></h1>
+        <video
+          className="gta-video col-span-12 rounded-md"
+          src={gta}
+          muted
+          autoPlay
+          typeof="video/mp4"
+        ></video>
+      </div>
     </>
   );
 }
