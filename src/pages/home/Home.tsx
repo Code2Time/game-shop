7e44bdcd512fa459fec42784a90e2b6fd78532f4
Amir-Thp <code2time@gmail.com>
2024-04-09 10:22:18 +0330

    i axios and create api file

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import './Home.css'
+import { getproducts } from '../../services/api'
 function Home() {
+
+ const [products , setProducts]=useState([]) ;
+
+useEffect(() =>{
+ getproducts()
+ .then((result) => {
+  setProducts(result)
+ });
+ console.log(products)
+
+},[])
   return (
   <>
   <div data-aos="fade-up" data-aos-anchor-placement="top-center" className='header-container'>
