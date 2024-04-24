c57b9292d360d865d7ee9ab3cd9cb7a85de82b3f
Amir-Thp <code2time@gmail.com>
2024-04-24 09:03:32 +0330

    update shop page (product)

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { Iproducts } from "../../server/Server"
 import { useEffect, useState } from "react"
 import axios from "axios"
+import Navbarpro from "../navbarpro/Navbarpro";
 
 function Product() {
 const params = useParams<{id : string}>();
@@ -15,8 +16,28 @@ axios.get(`http://localhost:3000/products/${params.id}`).then((res) =>{
 
   return (
     <div>
-      <img src={product?.image} alt="" />
-    </div>
+      <Navbarpro />
+      <div className="shop-product-container max-w-[1000px] w-10/12 h-[300px] mx-auto grid grid-cols-12  my-52 shadow-lg my-3">
+        <div className="container-item col-span-12 sm:col-span-4  h-auto p-5"> 
+         <img className="rounded-md transition-all hover:-translate-y-1 hover:cursor-pointer" src={product?.image} alt="" />
+         
+        </div>
+        <div className="container-item col-span-12 sm:col-span-8  h-auto my-3">
+          <h1 className="col-span-12 mt-5 Lalezar border-r-4 px-2 border-orange-400 mx-4 hover:cursor-pointer">{product?.title}</h1>
+          <p className="p-5 my-10 text-justify">{product?.description}</p>
+        </div>
+        <div className="col-span-12 h-10 border">
+          <div className="button-container">
+            <button>+</button>
+            <button>حذف</button>
+            <button>-</button>
+          </div>
+          <div className="show-product">
+          
+          </div>
+        </div>
+      </div>
+      </div>
   )
 }
 
