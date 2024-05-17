7d6c24d173cc223df69774a50e0fde564a6f5897
Amir-Thp <code2time@gmail.com>
2024-05-17 19:24:36 +0330

    update Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,10 +3,25 @@ import "./Login.css";
 
 function Login() {
   return (
-    <>
+    <div className="login-container h-[100vh] w-full" data-aos='fade-up'>
       <Navbar />
-      login page
-    </>
+     <div className="wrapper mx-auto my-[250px]  ">
+        <form action="">
+            <h1 className='Lalezar'>ورود/ایجاد حساب</h1>
+            <div className="input-box">
+                <input  className='Lamia' type="text" placeholder="نام کاربری" required/>
+                
+                <i className='bx bxs-user'></i>
+            </div>
+            <div className="input-box">
+                <input  className='Lamia' type="password" placeholder="رمز عبور" required/>
+                <i className='bx bxs-lock-alt' ></i>
+            </div>
+              <button type="button"  className="btn Lamia text-white" >ورود</button>
+           
+        </form>
+    </div>
+    </div>
   );
 }
 
